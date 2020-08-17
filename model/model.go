2831e28acc12ef9bba61417746c09e5fbf5b8de1
zed <zedsmail@163.com>
2020-08-17 20:56:05 +0800

    feat: 解决snow并发下重复的问题

--- model/model.go
+++ model/model.go
@@ -1,9 +1,21 @@
 package model
 
 import (
+	"github.com/jinzhu/gorm"
 	"github.com/sony/sonyflake"
+	"sync"
 	"time"
 )
+
+var once sync.Once
+var snow *sonyflake.Sonyflake
+
+func GetSnow() *sonyflake.Sonyflake {
+	once.Do(func() {
+		snow = sonyflake.NewSonyflake(sonyflake.Settings{})
+	})
+	return snow
+}
 
 type HasId interface {
 	GetId() uint64
@@ -20,18 +32,13 @@ func (m *Model) GetId() uint64 {
 	return m.ID
 }
 
-func (m *Model) SetIDWithSnowFlake() {
-	s := sonyflake.NewSonyflake(sonyflake.Settings{})
-	id, err := s.NextID()
-	if err != nil {
-		panic(err)
-	}
-	m.ID = id
-}
-
-func (m *Model) BeforeSave() error {
+func (m *Model) BeforeSave(scope *gorm.Scope) error {
 	if m.ID == 0 {
-		m.SetIDWithSnowFlake()
+		id, err := GetSnow().NextID()
+		if err != nil {
+			panic(err)
+		}
+		scope.SetColumn("ID", id)
 	}
 	return nil
 }
