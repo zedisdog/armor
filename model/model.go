2dc43465bde8e765cc26eaadc13643f2ff597f69
zed <zedsmail@163.com>
2020-08-17 20:31:50 +0800

    feat: current_timestamp

--- model/model.go
+++ model/model.go
@@ -12,8 +12,8 @@ type HasId interface {
 
 type Model struct {
 	ID        uint64    `gorm:"type:bigint(20) UNSIGNED COMMENT '用户ID';PRIMARY_KEY;NOT NULL;" json:"id,string"`
-	CreatedAt time.Time `gorm:"type:datetime COMMENT '创建时间';NOT NULL;DEFAULT:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt time.Time `gorm:"type:datetime COMMENT '最后更新时间';NOT NULL;DEFAULT:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP" json:"updated_at"`
+	CreatedAt time.Time `gorm:"type:datetime COMMENT '创建时间' DEFAULT CURRENT_TIMESTAMP;NOT NULL" json:"created_at"`
+	UpdatedAt time.Time `gorm:"type:datetime COMMENT '最后更新时间' DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;NOT NULL;" json:"updated_at"`
 }
 
 func (m *Model) GetId() uint64 {
